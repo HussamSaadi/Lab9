8acd9b17ab859f7ffd80e7b7a001c51331184e84
ryan cecil <ryan.cecil@ufl.edu>
2024-04-10 11:06:44 -0400

    final push again

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ def encode(password):
     for item in password:
         item = int(item)
         item += 3
+        if item >= 10:
+            item = item -10
         new_pass.append(item)
     return new_pass
 def decode(password):
-    new_pass =[]
-    password =""
+    new_pass = []
     for item in password:
         item = int(item)
         item -= 3
+        if item <= 0:
+            item += 10
         new_pass.append(item)
     return new_pass
 
@@ -32,10 +35,16 @@ def main():
             new_pass = encode(password)
             print("Your password has been encoded and stored!")
         elif sel ==2:
-            new =""
+            new_str = ""
+            orig_pass =""
+            new = decode(new_pass)
             for item in new_pass:
-                new = new + str(item)
-            print(f"The encoded password is {new}, and the original password is {password}.")
+                new_str += str(item)
+            for item in new:
+                orig_pass += str(item)
+
+
+            print(f"The encoded password is {new_str}, and the original password is {orig_pass}.")
         elif sel == 3:
             break
 
